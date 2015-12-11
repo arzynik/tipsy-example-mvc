df58664792dffa8f900bceec84560c97915593ee
Devin Smith <github@arzynik.com>
2015-12-10 18:25:50 -0800

    updated mvc lib

--- app/controllers/api/user/index.php
+++ app/controllers/api/user/index.php
@@ -3,9 +3,12 @@
 namespace App\Controller\Api;
 
 class User extends \App\Rest {
-	public function init() {
-		header('Content-Type: application/json');
-		$user = new \App\User;
-		echo json_encode($user->data());
+	public function init($args = null) {
+		$this->inject(function($Request){
+			header('Content-Type: application/json');
+			// normaly you would use new \App\User($Request->loc(2)); when pulling for the db
+			$user = new \App\User;
+			echo json_encode($user->data());
+		});
 	}
 }
--- app/tipsy.php
+++ app/tipsy.php
@@ -5,3 +5,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Tipsy\Tipsy;
 Tipsy::config('../config/*.ini');
 Tipsy::run();
+
+Tipsy::router()
+	->when('api/user/:id', '\App\Controller\Api\User');
